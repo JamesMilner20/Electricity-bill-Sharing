0e2e2fc6146fa379f28afaff44384f58d6b4f36d
jamesmilner20 <jamesduah20@gmail.com>
2017-10-25 12:28:31 +0000

    Initial files as well as core css, html and JS written

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#enter1").click(function(event) {
+    event.preventDefault();
+
+    $("#identifier").show();
+    var inputtedNoHousehold = parseInt($("input#noHousehold").val());
+    for (var i = 1; i <= inputtedNoHousehold; i++){
+      $("#householdInfo").append(
+        '<form id="household" class="form-group">'+
+        '<div class="form-group col-md-7">'+
+        '<label for="bill">Household '+i+'</label>'+
+        '<input class="form-control" type="text" name="bill" placeholder="Name:">'+
+        '</div>'+
+        '<div class="form-group col-md-5">'+
+        '<label for="bill">Number of people in Household:</label>'+
+        '<input class="form-control bill" type="number" name="bill" placeholder="Number">'+
+        '</div>'+
+        '</form>')
+    };
+  });
+
+  $("#btn2").click(function(event) {
+    event.preventDefault();
+
+    var nos = $("input.bill")
+  });
+});
